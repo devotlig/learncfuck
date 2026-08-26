3e2e7bd0703e0ebaee1317abbb4d74dce06829bd
devotlig <1756138042@qq.com>
2026-08-26 16:50:39 +0800

    over94

---
+++ leetcode/code94.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // while(nodebegin->left!=nullptr)//先找到最左边的节点
+        // {
+        //     nodebegin = nodebegin->left;
+        // }
+        vector<int> ans;
+            if(root == nullptr)return {}; 
+            ans = inorderTraversal(root->left);
+            ans.push_back(root->val);
+            
+            vector<int> righttree = inorderTraversal(root->right);
+            ans.insert(ans.end(),righttree.begin(),righttree.end());
+            
+        
+        
+        return ans;
+    }
+};
+
+int main()
+{
+    TreeNode * root;
+    root = new TreeNode(1,nullptr,new TreeNode(2,new TreeNode(3),nullptr));
+    Solution s;
+    vector<int> Ans = s.inorderTraversal(root);
+    for(auto il = Ans.begin();il!=Ans.end();il++)
+    {
+        cout<<*il<<" ";
+    }
+}
