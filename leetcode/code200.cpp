3519b9d89bca693742e7a03529f4938d5dc8539e
devotlig <1756138042@qq.com>
2026-09-03 01:03:19 +0800

    code200begin

---
+++ leetcode/code200.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int landnum = 0;
+        
+    }
+};
+
+int main()
+{
+
+}
