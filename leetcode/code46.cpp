05cab804b060736ceff3ba225b7cca737176efef
devotlig <1756138042@qq.com>
2026-09-07 00:24:47 +0800

    code46begin

---
+++ leetcode/code46.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        return ans;
+    }
+};
+int main()
+{
+    vector<int> nums = {1,2,3};
+    Solution s;
+    vector<vector<int>> out;
+    out = s.permute(nums);
+    return 0;
+}
